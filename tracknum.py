f0943d50a6121b2298070131bbd2c980de7edd74
Gem Newman <spurll@gmail.com>
2022-10-15 16:11:20 -0500

    Fix when target dir isn't pwd; allow specifying a single file

--- tracknum.py
+++ tracknum.py
@@ -16,24 +16,43 @@ def main():
     parser = ArgumentParser(
         description='Adds (or overwrites) the ID3 track number in any MP3 file'
         'with a file name beginning with a number.')
-    parser.add_argument("dir", nargs='?',
-        help='The directory to process. Defaults to ".".', default='.')
+    parser.add_argument("dir", nargs='?', default='.',
+        help='The directory to process. Defaults to ".".')
+    parser.add_argument("--file", '-f', nargs='?', help='If provided, '
+        'processes a single file instead of the specified directory.')
+    parser.add_argument("--verbose", '-v', action='store_true')
     args = parser.parse_args()
 
-    # List all files in the dir
-    files = os.listdir(args.dir)
+    if args.file:
+        files = [args.file]
+    else:
+        # List all files in the dir
+        files = os.listdir(args.dir)
 
-    # Filter to only those that match the pattern
-    files = list(filter(p.match, files))
+        # Filter to only those that match the pattern
+        files = list(filter(p.match, files))
 
     for file in files:
+        if args.verbose:
+            print(file)
+
         track = int(p.match(file).group(1))
-        print(file)
+
+        if not track > 0:
+            if args.verbose:
+                print(f'Skipping invalid track number: {track}')
+            continue
+
+        # Append full path for files (since listdir only returns the filename)
+        if not args.file:
+            file = os.path.join(args.dir, file)
+
         audio = EasyID3(file)
         audio['tracknumber'] = str(track)
         audio.save()
 
-    print('Done')
+    if args.verbose:
+        print('Done')
 
 
 if __name__ == "__main__":
